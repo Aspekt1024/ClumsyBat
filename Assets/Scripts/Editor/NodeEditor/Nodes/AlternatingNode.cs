6ea0b4ae0a68469dff8011df1595a9bf712e68fd
DESKTOP-2N5MTEF\Dan <d.szabo87@gmail.com>
2017-06-03 18:46:08 +1000

    Introducing alternating action

---
+++ Assets/Scripts/Editor/NodeEditor/Nodes/AlternatingNode.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Ifaces = AlternatingAction.Ifaces;
+
+public class AlternatingNode : BaseNode {
+
+    protected override void AddInterfaces()
+    {
+        AddInput((int)Ifaces.Input);
+        AddOutput((int)Ifaces.Output1);
+        AddOutput((int)Ifaces.Output2);
+    }
+
+    private void SetInterfacePositions()
+    {
+        SetInterface((int)Ifaces.Input, 1);
+        SetInterface((int)Ifaces.Output1, 1);
+        SetInterface((int)Ifaces.Output2, 2);
+    }
+
+    public override void Draw()
+    {
+        WindowTitle = "Alternate";
+        WindowRect.size = new Vector2(80, 70);
+
+        SetInterfacePositions();
+        DrawInterfaces();
+    }
+
+    public override BaseAction GetAction()
+    {
+        return new AlternatingAction();
+    }
+}
---
+++ Assets/Scripts/EditorScripts/NodeEditor/Actions/AlternatingAction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlternatingAction : BaseAction {
+
+    public enum Ifaces
+    {
+        Input,
+        Output1, Output2
+    }
+
+    private bool firstOutput;
+
+    public override void ActivateBehaviour()
+    {
+        IsActive = false;
+
+        firstOutput = !firstOutput;
+        if (firstOutput)
+            CallNext((int)Ifaces.Output1);
+        else
+            CallNext((int)Ifaces.Output2);
+    }
+
+}
