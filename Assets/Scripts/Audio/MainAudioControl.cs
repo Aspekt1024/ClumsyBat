d2ed6ab997a4fe9ac79e35b322a84d597a8da146
DESKTOP-2N5MTEF\Dan <d.szabo87@gmail.com>
2017-06-04 02:24:00 +1000

    Added some sound effects

---
+++ Assets/Scripts/Audio/MainAudioControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainAudioControl : MonoBehaviour {
+
+    public AudioClip Shield;
+    public AudioClip BreakCrystal;
+    public AudioClip ClumsyRush;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlaySound(AudioClip sound)
+    {
+        audioSource.PlayOneShot(sound);
+    }
+}
