380f15bf979f98d196057c522ffbd656ee02362c
DESKTOP-2N5MTEF\Dan <d.szabo87@gmail.com>
2017-06-08 21:47:05 +1000

    TriggerClass.cs custom inspector added

---
+++ Assets/Scripts/Editor/LevelEditor/ObjectViews/TriggerObjectView.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+[CustomEditor(typeof(TriggerClass))]
+public class TriggerEditorView : Editor {
+
+    TriggerClass trigger;
+
+    public override void OnInspectorGUI()
+    {
+        trigger = (TriggerClass)target;
+
+        trigger.EventType = (TriggerHandler.EventType)EditorGUILayout.EnumPopup("Event Type", trigger.EventType);
+
+        if (trigger.EventType == TriggerHandler.EventType.OneTimeEvent || trigger.EventType == TriggerHandler.EventType.Dialogue)
+        {
+            trigger.EventId = (TooltipHandler.DialogueId)EditorGUILayout.EnumPopup("ID", trigger.EventId);
+        }
+        else
+        {
+            EditorStyles.textField.wordWrap = true;
+            trigger.TooltipText = EditorGUILayout.TextArea(trigger.TooltipText);
+        }
+        trigger.TooltipDuration = EditorGUILayout.FloatField("Duration (s)", trigger.TooltipDuration);
+        trigger.PausesGame = EditorGUILayout.Toggle("Pauses Game?", trigger.PausesGame);
+    }
+}
