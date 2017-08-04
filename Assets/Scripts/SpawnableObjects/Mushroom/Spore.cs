9309c9c5d513ec3e3cb93bb51a46e49a965db406
Dan-PC <d.szabo87@gmail.com>
2017-08-04 23:13:20 +1000

    mushroom spores

---
+++ Assets/Scripts/SpawnableObjects/Mushroom/Spore.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spore : MonoBehaviour {
+
+    private ParticleSystem sporeEffect;
+    private SpriteRenderer sporeSprite;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
