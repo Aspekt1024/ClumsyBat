b1cc66d68ca3b5b7439a853c8f6ae7aa4f2e9c39
Dan-PC <d.szabo87@gmail.com>
2017-10-17 00:14:23 +1100

    added the ability to unlock all levels and all abilities instantly (for dev purposes)

---
+++ Assets/Scripts/MenuScripts/MainMenu/DeveloperActions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeveloperActions : MonoBehaviour {
+
+    private LevelDataControl levelDataControl;
+
+    private void Start()
+    {
+        levelDataControl = GameData.Instance.Data.LevelData;
+    }
+
+    public void UnlockAllLevels()
+    {
+        levelDataControl.UnlockAllLevels();
+        Toolbox.Instance.MenuScreen = Toolbox.MenuSelector.LevelSelect;
+        SceneManager.LoadScene("Play");
+    }
+
+    public void UnlockAllAbilities()
+    {
+        GameData.Instance.Data.AbilityData.ActivateAllAbilities();
+    }
+}
