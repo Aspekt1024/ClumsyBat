8ec97c883203402242b7249db5b74c17f64418de
Dan-PC <d.szabo87@gmail.com>
2017-10-22 13:35:56 +1100

    Improved shield and knockback mechanic to give the player better control and stop ping-pong effect when bouncing between obstacles

--- Assets/Scripts/Player/Abilities/Shield.cs
+++ Assets/Scripts/Player/Abilities/Shield.cs
@@ -5,18 +5,19 @@ public class Shield : MonoBehaviour {
 
     private int _shieldCharges;
     private const int MaxCharges = 2;
-    private const float shieldDuration = 0.9f;
+    private const float SHIELD_DURATION = 1.1f;
+    private const float KNOCKBACK_DURATION = 0.55f;
 
     private Player _thePlayer;
     private Rigidbody2D _playerBody;
     private Lantern lantern;
     private ShieldEffect effect;
-    //private StatsHandler Stats;
 
     private enum ShieldStates
     {
         Idle,
-        Activated
+        Knockback,
+        Recovering
     }
     private ShieldStates _state = ShieldStates.Idle;
 
@@ -37,32 +38,50 @@ public class Shield : MonoBehaviour {
 
     private IEnumerator ShieldUp()
     {
-        _state = ShieldStates.Activated;
-        effect.BeginShieldEffect(shieldDuration);
+        _state = ShieldStates.Knockback;
+        effect.BeginShieldEffect(SHIELD_DURATION);
         _thePlayer.GetRenderer().color = new Color(0.6f, 0.6f, 1f);
-        // TODO animations
-        // PlayerAnim.Play("Knockback", 0, 0f);
+        SetPlayerColliders("PlayerIgnoreObstacles");
+
         yield return StartCoroutine(Knockback());
 
+        SetPlayerColliders("Player");
         _thePlayer.Anim.PlayAnimation(ClumsyAnimator.ClumsyAnimations.Hover);
 
         _state = ShieldStates.Idle;
         _thePlayer.GetRenderer().color = new Color(1f, 1f, 1f);
     }
 
+    private void SetPlayerColliders(string layerName)
+    {
+        int layerMask = LayerMask.NameToLayer(layerName);
+        Transform[] childTfArray = _thePlayer.GetComponentsInChildren<Transform>();
+
+        _thePlayer.gameObject.layer = layerMask;
+        for (int i = 0; i < childTfArray.Length; i++)
+        {
+            if (childTfArray[i].name == "Clumsy" || childTfArray[i].name == "ShieldObject")
+            {
+                childTfArray[i].gameObject.layer = layerMask;
+            }
+        }
+    }
+
     private IEnumerator Knockback()
     {
         float knockbackTimer = 0f;
-        const float knockbackDuration = shieldDuration;
         
         float directionModifier = GetDirectionModifier();
 
-        while (knockbackTimer < knockbackDuration)
+        while (knockbackTimer < SHIELD_DURATION)
         {
             if (!Toolbox.Instance.GamePaused)
             {
                 knockbackTimer += Time.deltaTime;
-                _playerBody.velocity = new Vector2(Mathf.Lerp(directionModifier * 7f, 0f, knockbackTimer/knockbackDuration), _playerBody.velocity.y);
+                if (knockbackTimer < KNOCKBACK_DURATION)
+                {
+                    _playerBody.velocity = new Vector2(Mathf.Lerp(directionModifier * 7f, 0f, knockbackTimer / SHIELD_DURATION), _playerBody.velocity.y);
+                }
             }
             yield return null;
         }
@@ -109,7 +128,7 @@ public class Shield : MonoBehaviour {
         return _shieldCharges;
     }
 
-    public bool IsAvailable() { return (_shieldCharges > 0 || _state == ShieldStates.Activated); }
-    public bool IsInUse() { return _state == ShieldStates.Activated; }
+    public bool IsAvailable() { return (_shieldCharges > 0 || _state == ShieldStates.Knockback); }
+    public bool IsInUse() { return _state == ShieldStates.Knockback || _state == ShieldStates.Recovering; }
 
 }
